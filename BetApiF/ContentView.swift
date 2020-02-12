0c2ce05c73cc2aaf1389410c977bf503d1d2fa20
Matthieu Habib <habib.matthieu1@gmail.com>
2020-02-12 19:03:04 +0100

    Initial Commit

---
+++ BetApiF/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetApiF
+//
+//  Created by MATTHIEU HABIB on 12/02/2020.
+//  Copyright © 2020 MATTHIEU HABIB. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
